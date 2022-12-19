47881d651192e452b5c43a9145af4a36f91d89db
Miku Fujiwara <llt9.oct22@gmail.com>
2022-12-18 17:00:49 -0800

    login: form done

--- pages/user_auth/login.php
+++ pages/user_auth/login.php
@@ -1,21 +1,45 @@
 <?php
-    include "./common/header.php"
+    include "../common/header.php"
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<div class="container-fluid">
+    <div class="row justify-content-center align-items-center g-2">
+        <div class="col-4">
+            <br>
+            <form method="post" action="">
+                <div class="form-floating mb-3">
+                    <input
+                        type="text"
+                        class="form-control" name="fname" placeholder="name">
+                    <label for="formId1">First Name</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                    <input
+                        type="text"
+                        class="form-control" name="lname" placeholder="name">
+                    <label for="formId1">Last Name</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                    <input
+                        type="email"
+                        class="form-control" name="email" placeholder="name">
+                    <label for="formId1">Email</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                    <input
+                        type="password"
+                        class="form-control" name="pass" placeholder="name">
+                    <label for="formId1">Password</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+
 
 
 <?php
-    include "./common/footer.php"
+    include "../common/footer.php"
 ?>
