e7f5a0b3c9af0e0ecccc3473ad65747bd83e69ac
Tyler Inari <github@al.tyler.jp>
2023-01-15 03:30:00 -0800

    managed api of articles

--- src/articles/edit_article.php
+++ src/articles/edit_article.php
@@ -24,6 +24,7 @@ function editContent($postInfo, $content)
 // OUTPUTS: statusCode, status, message
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $token = $_POST['token'];
+    $user_id = $_SESSION[$token]['id'];
     $title = $_POST['title'];
     $content = $_POST['content'];
     $genre_id_01 = $_POST['genre_id_01'];
@@ -32,59 +33,45 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $db = new dbServices($mysql_host, $mysql_username, $mysql_password, $mysql_database);
     $dbConnected = $db->connect();
     if ($dbConnected) {
-        $user = $dbConnected->query("SELECT `user_id` FROM `user_table` WHERE `token` = '$token'");
-        $user = $user->fetch_assoc();
-        if ($user) {
-            $user_id = $user['user_id'];
-            $postInfo = $dbConnected->query("SELECT `post_id`, `content_path` FROM `post_table` WHERE `user_id` = $user_id");
-            $postInfo = $postInfo->fetch_assoc();
-            if ($postInfo) {
-                $post_id = $postInfo['post_id'];
-                $content_path = $postInfo['content_path'];
-                $dbConnected->query("UPDATE `post_table` SET `title` = '$title', `genre_id_01` = $genre_id_01, `genre_id_02` = $genre_id_02, `genre_id_03` = $genre_id_03 WHERE `post_id` = $post_id");
-                $db->close();
-                $isEdit = editContent($postInfo, $content);
-                if ($isEdit) {
-                    echo "
+        $postInfo = $dbConnected->query("SELECT `post_id`, `content_path` FROM `post_table` WHERE `user_id` = $user_id");
+        $postInfo = $postInfo->fetch_assoc();
+        if ($postInfo) {
+            $post_id = $postInfo['post_id'];
+            $content_path = $postInfo['content_path'];
+            $dbConnected->query("UPDATE `post_table` SET `title` = '$title', `genre_id_01` = $genre_id_01, `genre_id_02` = $genre_id_02, `genre_id_03` = $genre_id_03 WHERE `post_id` = $post_id");
+            $db->close();
+            $isEdit = editContent($postInfo, $content);
+            if ($isEdit) {
+                echo "
                         {
                             \"statusCode\": 200,
                             \"status\": \"success\",
                             \"message\": \"Post edited successfully\"
                         }
                     ";
-                    exit();
-                } else {
-                    echo "
+                exit();
+            } else {
+                echo "
                         {
                             \"statusCode\": 500,
                             \"status\": \"Internal Server Error\",
                             \"message\": \"Post not edited\"
                         }
                     ";
-                    exit();
-                }
-            } else {
-                $db->close();
-                echo "
+                exit();
+            }
+        } else {
+            $db->close();
+            echo "
                     {
                         \"statusCode\": 404,
                         \"status\": \"Not Found\",
                         \"message\": \"Post not found\"
                     }
                 ";
-                exit();
-            }
-        } else {
-            $db->close();
-            echo "
-                {
-                    \"statusCode\": 401,
-                    \"status\": \"Unauthorized\",
-                    \"message\": \"Invalid token\"
-                }
-            ";
             exit();
         }
+
     } else {
         echo "
             {
